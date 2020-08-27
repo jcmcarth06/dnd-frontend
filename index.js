86a577d8db37b9f3095c7ecd8b3b78dd997caac2
jcmcarth06 <jcmcarth06@gmail.com>
2020-08-27 12:14:30 -0600

    add classes

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => alert('Welcome to the Fantasy Characters Storyboard! Use this resource to create, catalog, and share fantasy characters for public access to use in storytelling! Remember: any character you catalog is free to access for other writers, and any character someone else catalogs is up for grabs for use and/or inspiration!'))
 
+const charactersUrl = "http://localhost:3000/races"
+
+class Races {
+    constructor(name, image_link) {
+        this.name = name;
+        this.image_link = image_link
+    }
+}
+
+class Characters {
+    constructor(name, age, appearance, personality, background, affiliation) {
+        this.name = name;
+        this.age = age;
+        this.appearance = appearance;
+        this.personality = personality;
+        this.background = background;
+        this.affiliation = affiliation
+    }
+    createCharacter() {
+        const newCharacterButtons = document.createElement('race-buttons');
+
+    }
+}
