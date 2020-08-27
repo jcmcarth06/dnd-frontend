ac9d6db9e4176f1282e1d444ad6ca58d621da2f0
jcmcarth06 <jcmcarth06@gmail.com>
2020-08-27 15:18:07 -0600

    add const, name button ids

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => alert('Welcome to the Fantasy Characters Storyboard! Use this resource to create, catalog, and share fantasy characters for public access to use in storytelling! Remember: any character you catalog is free to access for other writers, and any character someone else catalogs is up for grabs for use and/or inspiration!'))
 
-const charactersUrl = "http://localhost:3000/races"
+const racesUrl = "http://localhost:3000/races"
+const charactersUrl = "http://localhost:3000/characters"
+const returnToRacesButton = document.getElementById("return-to-select-race")
+const returnToRaceButton = document.getElementById("return-to-view-race")
+const CreateCharacterButton = document.getElementById("create-character")
+const addCharacterButton = document.getElementById("add-character")
+const addRaceButton = document.getElementById("add-race")
 
 class Races {
     constructor(name, image_link) {
@@ -8,7 +14,9 @@ class Races {
         this.image_link = image_link
     }
     createRace() {
-        
+        const newRaceButton = document.createElement('div');
+        newRaceButton = 
+
     }
 }
 
