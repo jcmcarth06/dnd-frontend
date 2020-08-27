23935d888987a07a550dda29ea8cad1380c47d0b
jcmcarth06 <jcmcarth06@gmail.com>
2020-08-26 20:08:34 -0600

    add forms

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-document.addEventListener('DOMContentLoaded', () => alert('hi'))
+document.addEventListener('DOMContentLoaded', () => alert('Welcome to the Fantasy Characters Storyboard! Use this resource to create, catalog, and share fantasy characters for public access to use in storytelling! Remember: any character you catalog is free to access for other writers, and any character someone else catalogs is up for grabs for use and/or inspiration!'))
+
